6883e99f491ad79e00b2ada12a238d6489d7ccb4
matt22799 <matt2271999@hotmail.com>
2020-02-19 02:59:20 +0000

    Lab 2.1.1

--- main.go
+++ main.go
@@ -1,27 +1,44 @@
-// Programmer Name: Matthew Davenport
-// Date Created: 2/13/20
-// Program Description: Ask user for temp in Celsius, convert to Fahrenheit and Kelvin
-//                      then display back all values.
+// Programmer name: __________
+// Date completed:  __________
+// Description: ___________________________
 
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math/rand"
+    //"time"
+) //adding the ability to do random numbers
 
 func main() {
-  // ask for input
-    fmt.Println("Please enter your temperature in Celsius")
-    var celsius float64
-    fmt.Scanln(&celsius)
+    //create a variable for count\
+    var count int
 
-  // convert input to F
-    var fahrenheit float64 = (celsius * (9.0/5.0)) + 32
+    //ask the user to enter a max range for the guessing game and store that value in variable max.
+    var max int
+    fmt.Println("Please enter a a max range")
+    fmt.Scanln(&max)
+    
+    //this next line creates a random number from 1 to that guess for the computer to know.  You can test this by printing out the variable computerGuess
+    var computerGuess = rand.Intn(max)
 
-  // convert input to K
-    var kelvin float64 = celsius + 273.15
+    //ask the user to enter a guess for the computer number
+    var userGuess int
+    fmt.Println("Please enter a numerical guess")
+    fmt.Scanln(&userGuess)
 
-  // display back user input and conversions
-    fmt.Println("Your temperature in Celsius is", celsius)
-    fmt.Println("Your temperature in Fahrenheit is", fahrenheit)
-    fmt.Println("Your temperature in Kelvin is", kelvin)
+    //create a loop that compares the computerGuess to the userGuess while they are NOT equal go into the loop
+    for userGuess != computerGuess{
+      //increase the count by 1
+      count ++
+      //tell the user that the guessed incorrect
+      fmt.Println("Wrong!")
+      //ask the user to enter a new guess for the computer number
+      fmt.Println("Enter a new guess")
+      fmt.Scan(&userGuess)
+    }   
+    
+    //print out that the user got the answer correctly and how many guesses it took (the count)
+    fmt.Println("Correct! That took you", count, "tries!")
 
 }
