fb299734553cac1b947531da29e4d3b31f31bf11
matt22799 <matt2271999@hotmail.com>
2020-03-06 01:11:49 +0000

    Lab 2.5.2

--- main.go
+++ main.go
@@ -1,30 +1,104 @@
+// **********************************************************
 // Programmer name: Matthew Davenport
-// Date completed:  3/3/20
-// Description: This program will hold an array for items and another for prices, then  display back the price to each item.
-
+// Date completed:  3/4/20
+// Description: This program will start by showing a broken
+//              array with the users input, then it will
+//              display an array and calculate the biggest
+//              and smallest number, then finally will Ask
+//              for baseball players and stats, then compare
+//              the stats.
+// **********************************************************
 package main
 
 import "fmt"
 
 func main() {
-    //Create a string array of at least 5 values.  It should hold 5 products to buy
-    var items [5] string
-    items [0] = "milk"
-    items [1] = "eggs"
-    items [2] = "butter"
-    items [3] = "chicken"
-    items [4] = "salt"
-    //Create a float array of at least 5 values.  It should hold a price for each of the products
-    var prices [5] float64
-    prices [0] = 3.25
-    prices [1] = 2.39
-    prices [2] = .75
-    prices [3] = 5.99
-    prices [4] = 1.0
-    //Iterate through the array and output the product and it's price (similar to a menu)
-    fmt.Println(items[0], ": $", prices[0])
-    fmt.Println(items[1], ": $", prices[1])
-    fmt.Println(items[2], ": $", prices[2])
-    fmt.Println(items[3], ": $", prices[3])
-    fmt.Println(items[4], ": $", prices[4])
+// Broken Array
+fmt.Println("Broken Array")
+  // Set an array
+    var array [10] int
+  // Ask for number
+    var num int
+    for i := 1; i <= 9; i++ {
+      fmt.Println("Enter a number")
+      fmt.Scanln(&num)
+      array[i] = num 
+    }
+  // Display back
+    fmt.Println(array)
+
+// Max and Min
+fmt.Println("Max and Min")
+	// Set an array of numbers
+	a := [10]int {22,68,45,79,52,58,21,1,5,55}
+	// Calculate min and max
+	var min int = a[0]
+	var max int = a[0]
+
+	for _, v := range a {
+		if v < min {
+		min = v
+		}
+		if v > max {
+		max = v
+		}
+	}
+
+	// Display back max and min
+  fmt.Println(a)
+	fmt.Println("Biggest number is", max)
+	fmt.Println("Smallest numbers is", min)
+
+// Baseball Stats
+fmt.Println("Baseball Stats")
+	// Define arrays
+	var names [5] string
+	var average [5] float64
+	var slugging [5] float64
+
+	// Store valus into arrays
+	for i := 0; i<= 4; i++ {
+		fmt.Println("Enter a players name")
+		fmt.Scanln(&names[i])
+		fmt.Println("Enter that players batting average")
+		fmt.Scanln(&average[i])
+		fmt.Println("Enter that players slugging percentage")
+		fmt.Scanln(&slugging[i])
+	}
+
+	// Display back stats
+	for n := 0; n <= 4; n++ {
+		fmt.Println("Name:",names[n],"Batting Average:", average[n],"Slugging Percentage:", slugging[n])
+	}
+
+	// Compare batting averages 
+	var small float64 = average[0] 
+	var big float64= average[0]
+
+	for _, value := range average {
+		if value < small {
+		small = value
+		}
+		if value > big {
+		big = value
+		}
+	}
+	fmt.Println("Best batting average is", big)
+	fmt.Println("Worst batting average is", small)
+
+	// Compare slugging percentages
+	var worst float64 = slugging[0] 
+	var best float64= slugging[0]
+
+	for _, total := range slugging {
+		if total < worst {
+		worst = total
+		}
+		if total > best {
+		best = total
+		}
+	}
+	fmt.Println("Best slugging percentage is", best)
+	fmt.Println("Worst slugging percentage is", worst)
+
 }
