c75c7b536416dc9e0fa7587d97efa843a7321a76
matt22799 <matt2271999@hotmail.com>
2020-04-23 22:46:23 +0000

    Final Project 25% check

--- main.go
+++ main.go
@@ -1,100 +1,77 @@
-// Programmer name: Matthew Davenport
-// Date completed:  4/15/20
-// Description: This program will start by asking the user what kind of coffee they want, followed by asking if they want addons and what they want. Then it will run the corresponding function and display back the instructions, addons, and total cost
-
 package main
 
-import "fmt"
+import ( 
+      "fmt"
+      "math/rand"
+      "time"
+)
 
-// Create functions for each type of coffee using parameters for extras and addons
+// create variables for player and house hands
+var playerHand int = 0
+var houseHand int = 0
+var cardValue int
 
-func frappe(extras_ float64, addons_ string){
-  cost_ := 5.25
-  extra_ := extras_ * 1
-  total_ := (cost_ + extra_)
-  instructions := "Prepare espresso then chill, combine espresso with milk, sugar and ice in blender, blend until smooth, then pour and serve"
-  addedInstructions := addons_
-  
-  fmt.Println("Instructions:", instructions)
-  fmt.Println("Addons:", addedInstructions)
-  fmt.Println("Your total cost is", total_)
+// create function to add a card to players hand when “hit”
+func playerDraw(){
+    rand.Seed(time.Now().UnixNano())
+    cardValue = (rand.Intn(10) + 1)
+    playerHand = playerHand + cardValue
+    fmt.Println("Your hand value is", playerHand)
 }
-
-func latte(extras_ float64, addons_ string){
-  cost_ := 3.50
-  extra_ := extras_ * 1
-  total_ := (cost_ + extra_)
-  instructions := "Brew espresso, heat milk, combine milk and espresso, top with foam and serve"
-  addedInstructions := addons_
-  
-  fmt.Println("Instructions:", instructions)
-  fmt.Println("Addons:", addedInstructions)
-  fmt.Println("Your total cost is", total_)
-}
-
-func flatWhite(extras_ float64, addons_ string){
-  cost_ := 2.75
-  extra_ := extras_ * 1
-  total_ := (cost_ + extra_)
-  instructions := "Brew espresso, heat milk, combine milk and espresso, top with foam, fold foam and serve"
-  addedInstructions := addons_
-  
-  fmt.Println("Instructions:", instructions)
-  fmt.Println("Addons:", addedInstructions)
-  fmt.Println("Your total cost is", total_)
-}
-
-func cappuccino(extras_ float64, addons_ string){
-  cost_ := 2.50
-  extra_ := extras_ * 1
-  total_ := (cost_ + extra_)
-  instructions := "Brew espresso, heat milk, combine milk and espresso, top with foam and serve"
-  addedInstructions := addons_
-  
-  fmt.Println("Instructions:", instructions)
-  fmt.Println("Addons:", addedInstructions)
-  fmt.Println("Your total cost is", total_)
-}
-
-func macchiato(extras_ float64, addons_ string){
-  cost_ := 3.99
-  extra_ := extras_ * 1
-  total_ := (cost_ + extra_)
-  instructions := "Brew espresso, heat milk, combine milk and espresso, top with foam and serve"
-  addedInstructions := addons_
-  
-  fmt.Println("Instructions:", instructions)
-  fmt.Println("Addons:", addedInstructions)
-  fmt.Println("Your total cost is", total_)
+// create function to add a card to house's hand until its 17 or more
+func houseDraw(){
+  rand.Seed(time.Now().UnixNano())
+  cardValue = (rand.Intn(10) + 1)
+  houseHand = houseHand + cardValue
+  fmt.Println("House hand value is", houseHand)
 }
 
 func main() {
-// Collect user input
-  var input string
-  var extra float64
-  var addons string
-  // ask for user order
-  fmt.Println("What would you like to order?(Frappe, Latte, Flat White, Cappucino, Macchiato)")
-  fmt.Scanln(&input)
-  // ask for any extras
-  fmt.Println("Would you like anything extra? ($1 to add extras; enter 0 if no, 1 if yes)")
-  fmt.Scanln(&extra)
-  // if yes, ask what kind of extras
-  if (extra == 1) {
-    fmt.Println("What would you like to add?")
-    fmt.Scanln(&addons)
+  // add 2 cards to player hand and add
+  rand.Seed(time.Now().UnixNano())
+  cardValue = (rand.Intn(10) + 1)
+  playerHand = playerHand + cardValue
+  cardValue = (rand.Intn(10) + 1)
+  playerHand = playerHand + cardValue
+  
+  // add 2 cards to house hand and add
+  cardValue = (rand.Intn(10) + 1)
+  houseHand = houseHand + cardValue
+  cardValue = (rand.Intn(10) + 1)
+  houseHand = houseHand + cardValue
+
+  // display back current hands
+  fmt.Println("Your hand value is",playerHand)
+  fmt.Println("The house's hand value is",houseHand) 
+
+  // loop for players turn; if hit, run playerDraw; if stay, display final value
+  var choice string
+  if (playerHand <= 21){
+    for playerHand < 21 {
+      fmt.Println("Do you want to hit or stay?")
+      fmt.Scanln(&choice)
+      if (choice == "hit"){
+        playerDraw()
+      } else {
+        fmt.Println("Your final hand value is", playerHand)
+        break
+      }
+    }
   }
 
-// Display back instructions and cost
-  if (input == "Frappe"){
-    frappe(extra, addons)
-  } else if (input == "Latte"){
-    latte(extra, addons)
-  } else if (input == "Flat White"){
-    flatWhite(extra, addons)
-  } else if (input == "Cappuccino"){
-    cappuccino(extra, addons)
+  // loop for house's turn; ends when value is >= 17
+  for houseHand < 17 {
+    houseDraw()
+  }
+  // display house's final value
+  fmt.Println("The house's final hand value is", houseHand)
+
+  // compare player hand to house hand
+  if (playerHand > 21 || playerHand < houseHand && houseHand <= 21){
+    fmt.Println("You Lose!")
+  } else if (houseHand > 21 || houseHand < playerHand && playerHand <= 21){
+    fmt.Println("You Win!")
   } else {
-    macchiato(extra, addons)
+    fmt.Print("Draw!")
   }
 }
