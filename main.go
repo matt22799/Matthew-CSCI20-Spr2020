5d8047a632c3acede8b6e5450a87d874d47c1079
matt22799 <matt2271999@hotmail.com>
2020-05-01 18:24:03 +0000

    Project #3 updated

--- main.go
+++ main.go
@@ -1,106 +1,99 @@
 // Programmer name: Matthew Davenport
-// Date completed:  5/1/20
-// Description: This program will start by creating structs for animals, keepers, and zones then initialize each object within a zone object. Then, it will create a struct for inventory and ask the user what they want and then finally update the new inventory
+// Date completed:  4/15/20
+// Description: This program will start by asking the user what kind of coffee they want, followed by asking if they want addons and what they want. Then it will run the corresponding function and display back the instructions, addons, and total cost
 
 package main
 
 import "fmt"
 
-// Struct for animal
-  type Animal struct {
-    name string
-    species string
-    age int
-    breakfastHour int 
-    dinnerHour int
-  }
-// struct for keeper
-  type Keeper struct {
-    name string
-    zone string
-    animals int 
-  }
-// struct for zone
-  type Zone struct {
-    cleaner Keeper
-    animal1 Animal
-    animal2 Animal
-  }
+// Create functions for each type of coffee using parameters for extras and addons
+func frappe(extras_ float64)(float64){
+  cost_ := 5.25
+  extra_ := extras_ * 1
+  total_ := (cost_ + extra_)
 
-// struct for inventory
-  type Inventory struct {
-    partNumber int
-    unitPrice float64
-    quantity int
-  }
+  return total_
+}
+
+func latte(extras_ float64)(float64){
+  cost_ := 3.50
+  extra_ := extras_ * 1
+  total_ := (cost_ + extra_)
+
+  return total_
+}
+
+func flatWhite(extras_ float64)(float64){
+  cost_ := 2.75
+  extra_ := extras_ * 1
+  total_ := (cost_ + extra_)
+
+  return total_
+}
+
+func cappuccino(extras_ float64)(float64){
+  cost_ := 2.50
+  extra_ := extras_ * 1
+  total_ := (cost_ + extra_)
+
+  return total_
+}
+
+func macchiato(extras_ float64)(float64){
+  cost_ := 3.99
+  extra_ := extras_ * 1
+  total_ := (cost_ + extra_)
+
+  return total_
+}
 
 func main() {
-// Zoo Two
-  // zone objects that initialize keeper objects and animal objects
-    zoneMammal := Zone{
-      cleaner: Keeper{"Joe", "Mammal", 6},
-      animal1: Animal{"Tony", "Tiger", 4, 8, 6},
-      animal2: Animal{"Pepe", "Mule", 6, 7, 5},
-    }
+// Collect user input
+  var input string
+  var extra float64
+  var addons string
 
-    zoneReptile := Zone{
-      cleaner: Keeper{"Abby", "Reptile", 8},
-      animal1: Animal{"Momo", "Komodo Dragon", 9, 8, 6},
-    }
+  // ask for user order
+  fmt.Println("What would you like to order?(Frappe, Latte, FlatWhite, Cappuccino, Macchiato)")
+  fmt.Scanln(&input)
+  // ask for any extras
+  fmt.Println("Would you like anything extra? ($1 to add extras; enter 0 if no, 1 if yes)")
+  fmt.Scanln(&extra)
+  // if yes, ask what kind of extras
+  if (extra == 1) {
+    fmt.Println("What would you like to add?")
+    fmt.Scanln(&addons)
+  }
 
-    zoneBird := Zone{
-      cleaner: Keeper{"Anthony", "Bird", 7},
-      animal1: Animal{"Patchy", "Parrot", 2, 5, 4},
-      animal2: Animal{"Snowball", "Penguin", 6, 9, 6},
-    }
+  // Display back instructions and cost
+  if (input == "Frappe"){
+    fmt.Println("Prepare espresso then chill, combine espresso with milk, sugar and ice in blender, blend until smooth, then pour and serve")
+    fmt.Println("Add extra", addons) 
+    order := frappe(extra)
+    fmt.Println("Total Price:", order)  
 
-  // display Zone objects
-    fmt.Println("Mammal zone info:", zoneMammal)
-    fmt.Println("Reptile zone info:", zoneReptile)
-    fmt.Println("Bird zone info:", zoneBird)
+  } else if (input == "Latte"){
+    fmt.Println("Brew espresso, heat milk, combine milk and espresso, top with foam and serve")
+    fmt.Println("Add extra", addons) 
+    order := latte(extra)
+    fmt.Println("Total Price:", order)   
 
+  } else if (input == "FlatWhite"){
+    fmt.Println("Brew espresso, heat milk, combine milk and espresso, top with foam and serve")
+    fmt.Println("Add extra", addons) 
+    order := flatWhite(extra)
+    fmt.Println("Total Price:", order)  
 
-// Inventory
-  // create initialized objects of Inventory
-    snack := Inventory{
-      partNumber: 789,
-      unitPrice: 3.5,
-      quantity: 4,
-    }
-    meal := Inventory{
-      partNumber: 256,
-      unitPrice: 8.99,
-      quantity: 7,
-    }
-    drink := Inventory{
-      partNumber: 782,
-      unitPrice: 1.25,
-      quantity: 10,
-    }
+  } else if (input == "Cappuccino"){
+    fmt.Println("Brew espresso, heat milk, combine milk and espresso, top with foam and serve")
+    fmt.Println("Add extra", addons)  
+    order := cappuccino(extra)
+     fmt.Println("Total Price:", order)  
 
-  // display full inventory
-    fmt.Println("Snacks:", snack.quantity)
-    fmt.Println("Meals:", meal.quantity)
-    fmt.Println("Drinks:", drink.quantity)
-
-  // ask user what they are buying
-    var item string
-    fmt.Println("What would you like to buy?")
-    fmt.Scanln(&item)
-
-  // decide which object needs to be updated
-    if (item == "Snack" || item == "snack"){
-      snack.quantity--
-    } else if (item == "Meal" || item == "meal"){
-      meal.quantity--
-    } else if (item == "Drink" || item == "drink"){
-      drink.quantity--
-    } else {
-      fmt.Println("Sorry! We are sold out of that!")
-    }
-
-  // display updated inventory
-    fmt.Println("Snacks:", snack.quantity)
-    fmt.Println("Meals:", meal.quantity)
-    fmt.Println("Drinks:", drink.quantity)
+  } else {
+    fmt.Println("Brew espresso, heat milk, combine milk and espresso, top with foam and serve")
+    fmt.Println("Add extra", addons) 
+    order := macchiato(extra)
+    fmt.Println("Total Price:", order)  
+  }
 }
