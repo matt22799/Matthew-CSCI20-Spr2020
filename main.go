f827a70467ac5e0678f6553940713fd43e159fd4
null null <matt2271999@hotmail.com>
2020-05-26 16:57:14 +0000

    Lab 2.1.2 (updated)

--- main.go
+++ main.go
@@ -1,110 +1,48 @@
-// Programmer name: Matthew Davenport
-// Date completed:  5/11/20
-// Description: This program will start by asking the user what kind of coffee they want, followed by asking if they want addons and what they want. Then it will run the corresponding function and display back the instructions, addons, and total cost
-
 package main
 
 import "fmt"
 
-// create struct for coffee using cost and extra
-type Coffee struct {
-  cost_ float64
-  extra_ float64
-}
-
-// Create functions for each type of coffee using parameters for extras and addons
-func frappe(extras_ float64)(float64){
-  frappe_ := Coffee{
-    cost_: 4.25,
-    extra_: extras_ * 1,
-  }
-  total_ := (frappe_.cost_ + frappe_.extra_)
-  return total_
-}
-
-func latte(extras_ float64)(float64){
-  latte_ := Coffee{
-    cost_: 4.25,
-    extra_: extras_ * 1,
-  }
-  total_ := (latte_.cost_ + latte_.extra_)
-  return total_
-}
-
-func flatwhite(extras_ float64)(float64){
-  flatwhite_ := Coffee{
-    cost_: 4.25,
-    extra_: extras_ * 1,
-  }
-  total_ := (flatwhite_.cost_ + flatwhite_.extra_)
-  return total_
-}
-
-func cappuccino(extras_ float64)(float64){
-  cappuccino_ := Coffee{
-    cost_: 4.25,
-    extra_: extras_ * 1,
-  }
-  total_ := (cappuccino_.cost_ + cappuccino_.extra_)
-  return total_
-}
-
-func macchiato(extras_ float64)(float64){
-  macchiato_ := Coffee{
-    cost_: 4.25,
-    extra_: extras_ * 1,
-  }
-  total_ := (macchiato_.cost_ + macchiato_.extra_)
-  return total_
-}
-
 func main() {
-// Collect user input
-  var input string
-  var extra float64
-  var addons string
+// Average Quiz Score
+  total := 0
+  numGrades := 0
+  var grade int
 
-  // ask for user order
-  fmt.Println("What would you like to order?(Frappe, Latte, FlatWhite, Cappuccino, Macchiato)")
-  fmt.Scanln(&input)
-  // ask for any extras
-  fmt.Println("Would you like anything extra? ($1 to add extras; enter 0 if no, 1 if yes)")
-  fmt.Scanln(&extra)
-  // if yes, ask what kind of extras
-  if (extra == 1) {
-    fmt.Println("What would you like to add?")
-    fmt.Scanln(&addons)
+for grade > -1 {
+    fmt.Println("Enter a grade from 0-100")
+    fmt.Scanln(&grade)
+
+    if (grade > -1) {
+      total = total + grade
+      numGrades++
+    } else if (grade == -1){
+      average := total/numGrades
+      fmt.Println("Total Average Grade:", average)
+
+    } else {
+      fmt.Println("Invalid Response")
+    }
   }
 
-  // Display back instructions and cost
-  if (input == "Frappe"){
-    fmt.Println("Prepare espresso then chill, combine espresso with milk, sugar and ice in blender, blend until smooth, then pour and serve")
-    fmt.Println("Add extra", addons) 
-    order := frappe(extra)
-    fmt.Println("Total Price:", order)  
+// Obnoxious Child
+  // collect value
+  var age int
+  fmt.Println("How old is the child?")
+  fmt.Scanln(&age)
 
-  } else if (input == "Latte"){
-    fmt.Println("Brew espresso, heat milk, combine milk and espresso, top with foam and serve")
-    fmt.Println("Add extra", addons) 
-    order := latte(extra)
-    fmt.Println("Total Price:", order)   
+  // start loop based on value
+  for i := 0; i < age; i++{
+  fmt.Println("Are we there yet?")
+  }  
 
-  } else if (input == "FlatWhite"){
-    fmt.Println("Brew espresso, heat milk, combine milk and espresso, top with foam and serve")
-    fmt.Println("Add extra", addons) 
-    order := flatwhite(extra)
-    fmt.Println("Total Price:", order)  
+// Song Lyrics 
+  // Ask for number of repeats
+  var repeat int
+  fmt.Println("How many times do you want to hear the song?")
+  fmt.Scanln(&repeat)
 
-  } else if (input == "Cappuccino"){
-    fmt.Println("Brew espresso, heat milk, combine milk and espresso, top with foam and serve")
-    fmt.Println("Add extra", addons)  
-    order := cappuccino(extra)
-     fmt.Println("Total Price:", order)  
-
-  } else {
-    fmt.Println("Brew espresso, heat milk, combine milk and espresso, top with foam and serve")
-    fmt.Println("Add extra", addons) 
-    order := macchiato(extra)
-    fmt.Println("Total Price:", order)  
-  }
+  // start loop based on value
+  for i := 0; i < repeat; i++{
+  fmt.Println("We all live in a yellow submarine, a yellow submarine, a yellow submarine")
+  }  
 }
